2f9f63578538176224f4bab88dcf1019f10c50fc
Oluwaferanmi-Dev <feranmioyelowo@gmail.com>
2023-10-20 10:59:41 +0100

    My Portfolio

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<footer class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center">
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium">Connect with me &darr;</p>
+    <div class="flex flex-col gap-4 items-center justify-center">
+        <p><b class="pr-2">Email</b> feranmioyelowo@gmail.com</p>
+        <p>
+            <b class="pr-2">GitHub</b>
+            <a href="https://github.com/Oluwaferanmi-Dev" target="_blank" class="text-violet-400">
+                Oluwaferanmi-Dev
+                <sup class=""><span class="text-xs scale-75 pl-0.5"><i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i></span></sup>
+            </a>
+        </p>
+        <p>
+            <b class="pr-2">LinkedIn</b>
+            <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" class="text-violet-400">
+                your-linkedin-profile
+                <sup class=""><span class="text-xs scale-75 pl-0.5"><i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i></span></sup>
+            </a>
+        </p>
+    </div>
+</footer>

12256fddd617caab97eb4f50098556ab8c593f51
Caleb <92544582+Oluwaferanmi-Dev@users.noreply.github.com>
2024-11-13 01:20:03 +0100

    Update Main.svelte
    Added webpulse link

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -129,7 +129,7 @@ const links = {
                     
                    
                 </p>
-                <a href="https://caleb-to-do-app.vercel.app/" target="_blank">
+                <a href="https://webpulsev0.vercel.app/" target="_blank">
                     <button class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950">
                         <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200" />
                         <h4 class="relative z-9">Go to &rarr;</h4>
